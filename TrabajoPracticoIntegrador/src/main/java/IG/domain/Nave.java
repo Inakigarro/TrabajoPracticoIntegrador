b7c2de41aabc288f79b7455ea28ee0d6f4d8a7df
Iñaki Garro <ignakigarro@gmail.com>
2025-06-05 20:26:55 -0300

    Nueva clase `Nave` para administracion de zonas

---
+++ TrabajoPracticoIntegrador/src/main/java/IG/domain/Nave.java
@@ -0,0 +1,13 @@
+package IG.domain;
+
+public class Nave {
+    private Integer id;
+
+    public Nave() {
+        this.id = 0;
+    }
+
+    public Nave(Integer id) {
+        this.id = id;
+    }
+}
