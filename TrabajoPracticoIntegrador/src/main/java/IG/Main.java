eb049a3a2eef4c8af1b7ebad5ab081427e8ba020
Inakigarro <ignakigarro@gmail.com>
2025-08-31 20:24:08 -0300

    Actualizacion main

--- TrabajoPracticoIntegrador/src/main/java/IG/Main.java
+++ TrabajoPracticoIntegrador/src/main/java/IG/Main.java
@@ -1,10 +1,8 @@
 package IG;
 
-import IG.domain.Clases.TipoProducto;
+import IG.domain.DAO.ProductoUbicacionDAO;
 import IG.views.MainWindow;
 import IG.config.ConexionBD;
-import IG.domain.Clases.Producto;
-import IG.domain.DAO.ProductoDAO;
 
 import java.sql.Connection;
 
@@ -19,18 +17,6 @@ public class Main {
             System.out.println("Inicializando base de datos...");
             ProductoUbicacionDAO productoUbicacionDao = new ProductoUbicacionDAO(conn);
             productoUbicacionDao.inicializacion();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try (Connection conn = ConexionBD.obtenerConexion()) {
-            System.out.println("Conexión exitosa a la base de datos.");
-
-            System.out.println("Listando todos los productos:");
-            ProductoDAO listarDAO = new ProductoDAO(conn);
-            for (Producto producto : listarDAO.listarTodos()) {
-                System.out.println(producto);
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
